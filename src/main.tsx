a1fd5c412ef856f577392ebffda17e5676ce1907
withewolf-dev <itskashyap26@gmail.com>
2022-06-14 18:15:57 +0530

    query and axios config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
+import App from "./App";
+import "./index.css";
+import { queryClient } from "./utils/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
-)
+);
